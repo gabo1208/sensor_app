fa0e3474b2b415404051e4bcfe5b4695ef8ba3b1
Gabriel Freites <gabohd@gmail.com>
2018-05-03 16:13:51 -0400

    improved UI + added labels + check to empty emergency number to call

--- app/src/main/java/com/thesis/gabilloillo/sensorapp/MainActivity.java
+++ app/src/main/java/com/thesis/gabilloillo/sensorapp/MainActivity.java
@@ -140,24 +140,29 @@ public class MainActivity extends AppCompatActivity {
                         Snackbar.LENGTH_LONG).setAction("", null).show();
                 // Start emergency call
                 Intent callIntent = new Intent(Intent.ACTION_CALL); //use ACTION_CALL class
-                callIntent.setData(Uri.parse("tel:" + emergeny_number));    //this is the phone number calling
-                //check permission
-                //If the device is running Android 6.0 (API level 23) and the app's targetSdkVersion is 23 or higher,
-                //the system asks the user to grant approval.
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) !=
-                        PackageManager.PERMISSION_GRANTED) {
-                    //request permission from user if the app hasn't got the required permission
-                    ActivityCompat.requestPermissions(getParent(),
-                            new String[]{Manifest.permission.CALL_PHONE},   //request specific permission from user
-                            10);
-                    return;
-                }else {     //have got permission
-                    try{
-                        startActivity(callIntent);  //call activity and make phone call
+                if(!emergeny_number.equals("")) {
+                    callIntent.setData(Uri.parse("tel:" + emergeny_number));    //this is the phone number calling
+                    //check permission
+                    //If the device is running Android 6.0 (API level 23) and the app's targetSdkVersion is 23 or higher,
+                    //the system asks the user to grant approval.
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) !=
+                            PackageManager.PERMISSION_GRANTED) {
+                        //request permission from user if the app hasn't got the required permission
+                        ActivityCompat.requestPermissions(getParent(),
+                                new String[]{Manifest.permission.CALL_PHONE},   //request specific permission from user
+                                10);
+                        return;
+                    } else {     //have got permission
+                        try {
+                            startActivity(callIntent);  //call activity and make phone call
+                        } catch (android.content.ActivityNotFoundException ex) {
+                            Toast.makeText(getApplicationContext(),
+                                "SensorApp MainActivity was not found", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                    catch (android.content.ActivityNotFoundException ex){
-                        Toast.makeText(getApplicationContext(),"SensorApp MainActivity was not found",Toast.LENGTH_SHORT).show();
-                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "Please, enter an emergency number.",
+                        Toast.LENGTH_LONG).show();
                 }
             }
         });
